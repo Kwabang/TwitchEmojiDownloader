393dbacfe357ea744f394a3d3bd9c6e67982b8d5
Kwabang <kwabang2827@gmail.com>
2019-12-27 20:43:49 +0900

    Add count emotes list

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ with open('config.json') as json_file:
     id = json_data["twitch_user_id"]
 
 data = requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()
-emote = (data['emotes'][0]['id'])
-url = f"https://static-cdn.jtvnw.net/emoticons/v1/{emote}/3.0"
+total = len(data['emotes'])
+num = 
+emote_id = (data['emotes'][num]['id'])
+emote_name = (data['emotes'][num]['code'])
+url = f"https://static-cdn.jtvnw.net/emoticons/v1/{emote_id}/3.0"
 path = "img/"
-image_name = f"{emote}.png"
+image_name = f"{emote_name}.png"
 
 if __name__ == "__main__":
     urllib.request.urlretrieve(url, path+image_name)
