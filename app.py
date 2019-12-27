c5ed8d7aff54c7b1fa50d259be87380902defdde
Kwabang <kwabang2827@gmail.com>
2019-12-28 00:12:32 +0900

    Del variable & update download emotes script

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 import requests
 import json
 import urllib.request
-
 with open('config.json') as json_file:
     json_data = json.load(json_file)
     id = json_data["twitch_user_id"]
+    path = json_data["path"]
 
-data = requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()
-total = len(data['emotes'])
-num = 
-emote_id = (data['emotes'][num]['id'])
-emote_name = (data['emotes'][num]['code'])
-url = f"https://static-cdn.jtvnw.net/emoticons/v1/{emote_id}/3.0"
+num = 0
 path = "img/"
-image_name = f"{emote_name}.png"
 
 if __name__ == "__main__":
-    urllib.request.urlretrieve(url, path+image_name)
+    while True:
+        emote_id = (requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()['emotes'][num]['id'])
+        emote_name = (requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()['emotes'][num]['code'])
+        url = f"https://static-cdn.jtvnw.net/emoticons/v1/{emote_id}/3.0"
+        image_name = f"{emote_name}.png"
+        urllib.request.urlretrieve(url, path+image_name)
+        num += 1
+        if num == len(requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()['emotes']):
+            break
