2a239545e62af6dc82344d779604add400ee05d6
Kwabang <kwabang2827@gmail.com>
2019-12-27 20:15:41 +0900

    Add image downloader

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import requests
 import json
+import urllib.request
 
 with open('config.json') as json_file:
     json_data = json.load(json_file)
     id = json_data["twitch_user_id"]
+
 data = requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()
+emote = (data['emotes'][0]['id'])
+url = f"https://static-cdn.jtvnw.net/emoticons/v1/{emote}/3.0"
+path = "img/"
+image_name = f"{emote}.png"
 
 if __name__ == "__main__":
-    print (data["emotes"])
+    urllib.request.urlretrieve(url, path+image_name)
