001cdc9f55a1144cc17571173c70e451a46e98b3
Kwabang <kwabang2827@gmail.com>
2019-12-28 13:04:27 +0900

    Update inject id system

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ import json
 import urllib.request
 with open('config.json') as json_file:
     json_data = json.load(json_file)
-    id = json_data["twitch_user_id"]
     path = json_data["path"]
 
-num = 0
-
 if __name__ == "__main__":
+    streamer = input("다운로드를 원하는 스트리머의 ID를 입력해주세요. : ")
+    api = requests.get(f"https://api.twitch.tv/kraken/users?login={streamer}", params={'client_id':'prskmctggbzbblwr7tul4c4nuaraoc'}, headers={'Accept':'application/vnd.twitchtv.v5+json'}).json()
+    id = (api['users'][0]['_id'])
+    num = 0
     while True:
         emote_id = (requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()['emotes'][num]['id'])
         emote_name = (requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()['emotes'][num]['code'])
