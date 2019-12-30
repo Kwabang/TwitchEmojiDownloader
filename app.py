aa905ce3d5bdcfeb28c5bea78030c57e806f4e9d
Kwabang <kwabang2827@gmail.com>
2019-12-31 00:12:06 +0900

    Upgrade save system

--- app.py
+++ app.py
@@ -13,13 +13,15 @@ if __name__ == "__main__":
     streamer = input("다운로드를 원하는 스트리머의 ID를 입력해주세요. : ")
     id = requests.get("https://api.twitch.tv/helix/users?login="+streamer, headers={'Client-ID': 'prskmctggbzbblwr7tul4c4nuaraoc'}).json()['data'][0]['id']
     num = 0
+    if not(os.path.isdir(f"{path}{streamer}")):
+            os.makedirs(os.path.join(f"{path}{streamer}"))
     while True:
         emote_id = (requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()['emotes'][num]['id'])
         emote_name = (requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()['emotes'][num]['code'])
         url = f"https://static-cdn.jtvnw.net/emoticons/v1/{emote_id}/3.0"
         image_name = f"{emote_name}.png"
         print (f"Downloading {emote_name}")
-        urllib.request.urlretrieve(url, path+image_name)
+        urllib.request.urlretrieve(url, f"{path}{streamer}/{image_name}")
         num += 1
         if num == len(requests.get('https://api.twitchemotes.com/api/v4/channels/'+id).json()['emotes']):
             break
