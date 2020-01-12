ec377ec8f6f25639d5087f34eee83e8e2aa0ac87
Kwabang <43908654+Kwabang@users.noreply.github.com>
2020-01-13 02:57:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ with open('config.json') as json_file:
     json_data = json.load(json_file)
     path = json_data["path"]
 
-if not(os.path.isdir(f"{path}")):
-    os.makedirs(os.path.join(f"{path}"))
-
 if __name__ == "__main__":
     streamer = input("다운로드를 원하는 스트리머의 ID를 입력해주세요. : ")
     id = requests.get("https://api.twitch.tv/helix/users?login="+streamer, headers={'Client-ID': 'prskmctggbzbblwr7tul4c4nuaraoc'}).json()['data'][0]['id']
